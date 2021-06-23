caa8fef1952a1445d47db3dcb37c080ed1ed5273
Adrien-Ray <forpub@accesdenied.net>
2021-06-23 11:43:29 +0200

    debug

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+include "includes/header.php";
+?>
+
+
+
+
+<!-- DIV BLEU -->
+
+
+<div class="zoneblue">
+    <!-- ici insertion img position absolute (+divbleu = position relative) et z-index -->
+
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3 col-sm-1 border-ad centerback"><img src="<?php echo get_template_directory_uri(); ?>/assets/pictures/img-blue-left-opacity100.png" alt="deux personnes se serrant la main" class="back-img blueleft"></div>
+            <div class="col-md-6 col-sm-10 border-ad">
+                <h1 class="h1-blue">Find the right partners to fuel your business growth</h1>
+                <p class="p-bluezone">Join a vibrant community of MSPs to forge long-lasting relationships with partners that help you create excellent customer experiences</p>
+                <p class="button-zonebleu"><a href="#" class="button-zonebleu">Register today</a></p>
+            </div>
+            <div class="col-md-3 col-sm-1 border-ad centerback"><img src="<?php echo get_template_directory_uri(); ?>/assets/pictures/img-blue-right-opacity100.png" alt="une personne s'enrichissant" class="back-img blueright"></div>
+        </div>
+    </div>
+</div>
+<div class="zoneblue2"></div>
+
+
+<!-- FIN DIV BLEU -->
+
+<div class="backfix">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/pictures/back-titre-shell.png" alt="decoration" class="back-fix-left">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/pictures/back-titre-shell.png" alt="decoration" class="back-fix-right">
+</div>
+
+<!-- CONTENTFLUID -->
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 col-sm-12 content">
+            <h1>List. Sell. Grow</h1>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-4 col-sm-12 cardlistshellgrow">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/pictures/logo-shell-1.png" alt="logo Be a part of the only All-In-One platform for IT services" class="">
+                        <p>Be a part of the only All-In-One platform for IT services</p>
+                    </div>
+                    <div class="col-md-4 col-sm-12 cardlistshellgrow">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/pictures/logo-shell-2.png" alt="logo Come closer to finding your next customer" class="">
+                        <p>Come closer to finding your next customer</p>
+                    </div>
+                    <div class="col-md-4 col-sm-12 cardlistshellgrow">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/pictures/logo-shell-3.png" alt="logo Grow your business with a single click" class="">
+                        <p>Grow your business with a single click</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 contentline1">
+                        <H2 class="contentline">Your channel program on steroids</H2>
+                        <p class="contentline">Tired of finding MSPs that can help you boost your channel sales? You’re in luck, because they’re all here.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 contentline2">
+                        <H2 class="contentline">Become the vendor everyone wants to buy from</H2>
+                        <p class="contentline">Gain brand authority and visibility with the help of the largest IT service ecosystem. Find customers who are looking for your solution right now!</p>
+                        <p class="singup"><a href="#">Singup now -></a></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 contentline3">
+                        <H2 class="contentline">It’s like having Your best Salesman on autopilot</H2>
+                        <p class="contentline">Share collaterals and documents that are automatically branded. Leverage a rich partner network that sells your solution exactly the way your best salesman would.</p>
+                        <p class="singup"><a href="#">Singup now -></a></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 contentline4">
+                        <H2 class="contentline">Track your channel performance</H2>
+                        <p class="contentline">Monitor the health and revenue of your channel program and streamline your campaign to win more customers within Zomentum.</p>
+                        <p class="singup"><a href="#">Singup now -></a></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 contentline5">
+                        <H2 class="contentline">Get The best version of Your sales playbook</H2>
+                        <p class="contentline">Gather insights about top channel sellers and find out what pitch and collaterals ensure maximum profitability. Win happy customers with an optimized sales playbook.</p>
+                        <p class="singup"><a href="#">Singup now -></a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- FIN CONTENTFLUID -->
+
+
+<?php include "includes/footer.php"; ?>
